7293b5cef31f408aedc7bf470ea2f3b7f09486c5
MateusdosAnjos <mateus.a.anjos@gmail.com>
2018-05-25 17:25:17 -0300

    Criado verificação de dados para uma nova conta, css anterior foi melhorado

--- Chatterzilla/app/controllers/users_controller.rb
+++ Chatterzilla/app/controllers/users_controller.rb
@@ -33,7 +33,9 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:login, :email, :password)
+    params.require(:user).permit(:login, :email, :password,
+                                 :password_confirmation)
+
   end
 
 end
