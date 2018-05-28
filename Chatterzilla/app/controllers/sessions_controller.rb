e49089b156ab28e35cf8eedd2bcec891ef4de37d
MateusdosAnjos <mateus.a.anjos@gmail.com>
2018-05-28 16:03:40 -0300

    Adicionado sistema inicial de login e sessions

--- Chatterzilla/app/controllers/sessions_controller.rb
+++ Chatterzilla/app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
-      redirect_to sessions_path
+      log_in user
+      redirect_to user
     else
-      render html: "Conta inexistente, favor criar uma conta!"
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
     end
   end
 
