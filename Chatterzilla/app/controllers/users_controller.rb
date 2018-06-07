0b6e20bc19adbcb496ac1e5ef8e1e3771a275836
MateusdosAnjos <mateus.a.anjos@gmail.com>
2018-06-07 18:53:27 -0300

    Agora um usuario pode editar apenas seu proprio perfil (bloqueio do metodo de edicao a partir de /users/id/edit)

--- Chatterzilla/app/controllers/users_controller.rb
+++ Chatterzilla/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def index
     @user = User.all
   end
@@ -45,7 +48,18 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:login, :email, :password,
                                  :password_confirmation)
+  end
 
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 
 end
