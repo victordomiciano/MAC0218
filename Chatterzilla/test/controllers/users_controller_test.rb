65a3cc453026da7b260e67c67aed158059eeb25e
MateusdosAnjos <mateus.a.anjos@gmail.com>
2018-06-09 11:01:57 -0300

    Adicionado modelo que mostra todos os usuarios ao estar logado (testes criados simultaneamente TDD)

--- Chatterzilla/test/controllers/users_controller_test.rb
+++ Chatterzilla/test/controllers/users_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user       = users(:mateus)
+    @other_user = users(:zetum)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
+
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
 end
