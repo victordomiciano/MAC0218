d799cfe9e692f5546d859e8f64378cb8c5cf953f
Victor Domiciano <victor.dm3@gmail.com>
2018-06-12 15:32:43 -0300

    Adiciona testes de rota

--- Chatterzilla/test/controllers/sessions_controller_test.rb
+++ Chatterzilla/test/controllers/sessions_controller_test.rb
@@ -2,4 +2,27 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "Chatterzilla"
+  end
+
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "Help | Chatterzilla"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Chatterzilla"
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Chatterzilla"
+  end
 end
