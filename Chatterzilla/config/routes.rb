ea9d62ce238e603eeb9cef0bf532783ded2b6128
Victor Domiciano <victor.dm3@gmail.com>
2018-06-12 16:04:53 -0300

    Corrige testes de integração

--- Chatterzilla/config/routes.rb
+++ Chatterzilla/config/routes.rb
@@ -4,9 +4,6 @@ Rails.application.routes.draw do
   root   'sessions#new'
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
-  get    '/help',    to: 'sessions#help'
-  get    '/about',   to: 'sessions#about'
-  get    '/contact', to: 'sessions#contact'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
--- Chatterzilla/test/controllers/sessions_controller_test.rb
+++ Chatterzilla/test/controllers/sessions_controller_test.rb
@@ -7,22 +7,4 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Chatterzilla"
   end
-
-  test "should get help" do
-    get help_path
-    assert_response :success
-    assert_select "title", "Help | Chatterzilla"
-  end
-
-  test "should get about" do
-    get about_path
-    assert_response :success
-    assert_select "title", "About | Chatterzilla"
-  end
-
-  test "should get contact" do
-    get contact_path
-    assert_response :success
-    assert_select "title", "Contact | Chatterzilla"
-  end
 end
