be82fe85dafda4d3a5e5469fb25c98a6f4a80321
MateusdosAnjos <mateus.a.anjos@gmail.com>
2018-06-19 14:55:46 -0300

    Adiciona o inicio do modelo de mensagens
    Signed-off-by: Victor Domiciano <victor.dm3@gmail.com>

--- Chatterzilla/app/controllers/users_controller.rb
+++ Chatterzilla/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
 
   def index
@@ -40,8 +40,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
