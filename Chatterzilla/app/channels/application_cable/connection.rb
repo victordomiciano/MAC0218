e233eed98ac6a2840b7a338b940cd727d45b0bbd
Nicolas <nicolas.nogueira@hotmail.com>
2018-06-28 19:11:12 -0300

    arruma alguns problemas dessa versao do chat

--- Chatterzilla/app/channels/application_cable/connection.rb
+++ Chatterzilla/app/channels/application_cable/connection.rb
@@ -3,14 +3,15 @@ module ApplicationCable
     identified_by :current_user
 
     def connect
-      self.current_user = find_verified_user
+      self.current_user = current_user
     end
 
     protected
 
     def find_verified_user
-      if (current_user = env['warden'].user)
-        current_user
+      verified_user = User.find_by(id: cookies.signed['current_user.id'])
+      if verified_user
+        verified_user
       else
         reject_unauthorized_connection
       end
